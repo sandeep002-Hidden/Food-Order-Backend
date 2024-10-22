2d13b40c57b37fd1a69e803ee80194ed3cd206d6
sandeep002-Hidden <21cse382.sandeepmohapatra@giet.edu>
2024-10-22 21:35:40 +0530

    confoguring cors

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ try {
 app.use(cookieParser())
 
 app.use(cors({
-  origin: [process.env.LOCAL,process.env.NATIVE,process.env.FORNTEND],
+  origin: ['https://ordernow-sandeepmohapatra.vercel.app/','http://192.168.6.182:8000','http://localhost:3000'],
   credentials: true,
 }));
 app.use(express.json());
