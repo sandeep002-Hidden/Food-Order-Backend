3d867239d228ac7d7be894ada1b0000aa4ac39e6
sandeep002-Hidden <21cse382.sandeepmohapatra@giet.edu>
2024-10-23 00:20:50 +0530

    "remove cookie

--- controllers/logout.js
+++ controllers/logout.js
@@ -1,7 +1,11 @@
-export default async function logout(req,res){
-    try {
-        return res.clearCookie("orderNow").json({message:"successfully logout",success:true})
-    } catch (error) {
-        return res.status(401).json({message:"error while logging out",success:false})
-    }
+export default async function logout(req, res) {
+  try {
+    return res
+      .clearCookie("orderNow", { sameSite: "None", secure: true })
+      .json({ message: "successfully logout", success: true });
+  } catch (error) {
+    return res
+      .status(401)
+      .json({ message: "error while logging out", success: false });
+  }
 }
